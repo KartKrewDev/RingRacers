8abfee5dd04dc5518059158f243850763c8bd1fd
TehRealSalt <tehrealsalt@gmail.com>
2019-05-21 01:18:08 -0400

    Increase SPB force distance

--- src/k_kart.c
+++ src/k_kart.c
@@ -1244,7 +1244,7 @@ static void K_KartItemRoulette(player_t *player, ticcmd_t *cmd)
 
 		// SPECIAL CASE No. 5:
 		// Force SPB onto 2nd if they get too far behind
-		if (player->kartstuff[k_position] == 2 && pdis > (DISTVAR*6)
+		if (player->kartstuff[k_position] == 2 && pdis > (DISTVAR*9)
 			&& spbplace == -1 && !indirectitemcooldown && !dontforcespb
 			&& cv_selfpropelledbomb.value)
 		{
