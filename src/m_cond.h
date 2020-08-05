c7d4be30f0b69b363dd678bc359467058dc84b54
Sally Coolatta <tehrealsalt@gmail.com>
2020-08-05 10:15:04 -0400

    Merge m_cond.c/h

--- src/m_cond.h
+++ src/m_cond.h
@@ -25,21 +25,10 @@ typedef enum
 	UC_POWERLEVEL,		// SRB2Kart: POWERLEVEL [power level to reach] [gametype, "0" for race, "1" for battle]
 	UC_GAMECLEAR,		// GAMECLEAR <x times>
 	UC_ALLEMERALDS,		// ALLEMERALDS <x times>
-	//UC_ULTIMATECLEAR,	// ULTIMATECLEAR <x times>
-	//UC_OVERALLSCORE,	// OVERALLSCORE [score to beat]
 	UC_OVERALLTIME,		// OVERALLTIME [time to beat, tics]
-	//UC_OVERALLRINGS,	// OVERALLRINGS [rings to beat]
 	UC_MAPVISITED,		// MAPVISITED [map number]
 	UC_MAPBEATEN,		// MAPBEATEN [map number]
-	UC_MAPALLEMERALDS,	// MAPALLEMERALDS [map number]
-	//UC_MAPULTIMATE,		// MAPULTIMATE [map number]
-	//UC_MAPPERFECT,		// MAPPERFECT [map number]
-	//UC_MAPSCORE,		// MAPSCORE [map number] [score to beat]
 	UC_MAPTIME,			// MAPTIME [map number] [time to beat, tics]
-	//UC_MAPRINGS,		// MAPRINGS [map number] [rings to beat]
-	//UC_NIGHTSSCORE,		// NIGHTSSCORE [map number] <mare, omit or "0" for overall> [score to beat]
-	//UC_NIGHTSTIME,		// NIGHTSTIME [map number] <mare, omit "0" overall> [time to beat, tics]
-	//UC_NIGHTSGRADE,		// NIGHTSGRADE [map number] <mare, omit "0" overall> [grade]
 	UC_TRIGGER,			// TRIGGER [trigger number]
 	UC_TOTALEMBLEMS,	// TOTALEMBLEMS [number of emblems]
 	UC_EMBLEM,			// EMBLEM [emblem number]
@@ -68,33 +57,17 @@ typedef struct
 } conditionset_t;
 
 // Emblem information
-<<<<<<< HEAD
-#define ET_GLOBAL 0 // Global map emblem, var == color
-#define ET_SKIN   1 // Skin specific emblem, var == skin
-//#define ET_SCORE  2
-#define ET_TIME   2
-//#define ET_RINGS  4
-//#define ET_NGRADE 5
-//#define ET_NTIME  6
-=======
-#define ET_GLOBAL 0 // Emblem with a position in space
-#define ET_SKIN   1 // Skin specific emblem with a position in space, var == skin
-#define ET_MAP    2 // Beat the map
-#define ET_SCORE  3 // Get the score
-#define ET_TIME   4 // Get the time
-#define ET_RINGS  5 // Get the rings
-#define ET_NGRADE 6 // Get the grade
-#define ET_NTIME  7 // Get the time (NiGHTS mode)
+#define ET_GLOBAL  0 // Emblem with a position in space
+#define ET_MAP     1 // Beat the map
+#define ET_TIME    2 // Get the time
+//#define ET_DEVTIME 3 // Time, but 
 
 // Global emblem flags
-#define GE_NIGHTSPULL 1 // sun off the nights track - loop it
-#define GE_NIGHTSITEM 2 // moon on the nights track - find it
+// (N/A to Kart yet)
+//#define GE_NIGHTSPULL 1
 
 // Map emblem flags
-#define ME_ALLEMERALDS 1
-#define ME_ULTIMATE    2
-#define ME_PERFECT     4
->>>>>>> srb2/next
+#define ME_ENCORE 1
 
 typedef struct
 {
@@ -133,21 +106,24 @@ typedef struct
 	UINT8 unlocked;
 } unlockable_t;
 
-// I have NO idea why these are going negative, but whatever.
-#define SECRET_NONE			-6 // Does nil.  Use with levels locked by UnlockRequired
-#define SECRET_ITEMFINDER	-5 // Enables Item Finder/Emblem Radar
-#define SECRET_EMBLEMHINTS	-4 // Enables Emblem Hints
-#define SECRET_PANDORA		-3 // Enables Pandora's Box
-#define SECRET_TIMEATTACK	-2 // Enables Time Attack on the main menu
-#define SECRET_BREAKTHECAPSULES	-1 // Enables Break the Capsules on the main menu
-#define SECRET_HEADER		 0 // Does nothing on its own, just serves as a header for the menu
-#define SECRET_LEVELSELECT	 1 // Selectable level select
-#define SECRET_WARP			 2 // Selectable warp
-#define SECRET_SOUNDTEST	 3 // Sound Test
-#define SECRET_CREDITS		 4 // Enables Credits
-#define SECRET_ENCORE		 5 // Enables Encore mode cvar
-#define SECRET_HELLATTACK	 6 // Map Hell in record attack
-#define SECRET_HARDSPEED	 7 // Enables Hard gamespeed
+#define SECRET_NONE			 0 // Does nil.  Use with levels locked by UnlockRequired
+#define SECRET_HEADER		 1 // Does nothing on its own, just serves as a header for the menu
+
+#define SECRET_LEVELSELECT	 2 // Selectable level select
+#define SECRET_WARP			 3 // Selectable warp
+#define SECRET_TIMEATTACK	 4 // Enables Time Attack on the main menu
+#define SECRET_BREAKTHECAPSULES	5 // Enables Break the Capsules on the main menu
+#define SECRET_SOUNDTEST	 6 // Sound Test
+#define SECRET_CREDITS		 7 // Enables Credits
+
+#define SECRET_ITEMFINDER	 8 // Enables Item Finder/Emblem Radar
+#define SECRET_EMBLEMHINTS	 9 // Enables Emblem Hints
+
+#define SECRET_ENCORE		10 // Enables Encore mode cvar
+#define SECRET_HARDSPEED	11 // Enables Hard gamespeed
+#define SECRET_HELLATTACK	12 // Map Hell in record attack
+
+#define SECRET_PANDORA		13 // Enables Pandora's Box
 
 // If you have more secrets than these variables allow in your game,
 // you seriously need to get a life.
