4b29dae0368b7b755941e17220e98218d7bae590
Sally Coolatta <tehrealsalt@gmail.com>
2020-08-10 17:06:12 -0400

    Fix extra AIMINGTODY

--- src/r_main.h
+++ src/r_main.h
@@ -28,9 +28,6 @@ extern fixed_t centerxfrac, centeryfrac;
 extern fixed_t projection, projectiony;
 extern fixed_t fovtan;
 
-// WARNING: a should be unsigned but to add with 2048, it isn't!
-#define AIMINGTODY(a) FixedDiv((FINETANGENT((2048+(((INT32)a)>>ANGLETOFINESHIFT)) & FINEMASK)*160), fovtan)
-
 extern size_t validcount, linecount, loopcount, framecount;
 
 //
