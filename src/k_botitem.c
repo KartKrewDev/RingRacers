5ac116dc12fc9dcf4b9389879ee7ff3842e050ed
Sally Coolatta <tehrealsalt@gmail.com>
2021-02-26 00:31:13 -0500

    Don't use rings during POSITION

--- src/k_botitem.c
+++ src/k_botitem.c
@@ -971,7 +971,7 @@ void K_BotItemUsage(player_t *player, ticcmd_t *cmd, INT16 turnamt)
 	{
 		// Use rings!
 
-		if (!player->exiting)
+		if (leveltime > starttime && !player->exiting)
 		{
 			K_BotItemRings(player, cmd);
 		}
