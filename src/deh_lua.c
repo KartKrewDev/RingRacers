9df5a4b25f700aa7064a47fb8cd96ebf337a43a5
James R <justsomejames2@gmail.com>
2021-02-27 03:15:00 -0800

    Conflicts a59c03fef 08d087c6b

--- src/deh_lua.c
+++ src/deh_lua.c
@@ -286,16 +286,6 @@ static inline int lib_getenum(lua_State *L)
 				lua_pushinteger(L, ((lua_Integer)1<<i));
 				return 1;
 			}
-		if (fastcmp(p, "FULLSTASIS"))
-		{
-			lua_pushinteger(L, (lua_Integer)PF_FULLSTASIS);
-			return 1;
-		}
-		else if (fastcmp(p, "USEDOWN")) // Remove case when 2.3 nears release...
-		{
-			lua_pushinteger(L, (lua_Integer)PF_SPINDOWN);
-			return 1;
-		}
 		if (mathlib) return luaL_error(L, "playerflag '%s' could not be found.\n", word);
 		return 0;
 	}
@@ -560,12 +550,6 @@ static inline int lib_getenum(lua_State *L)
 				return 1;
 			}
 		return 0;
-	}
-
-	if (fastcmp(word, "BT_USE")) // Remove case when 2.3 nears release...
-	{
-		lua_pushinteger(L, (lua_Integer)BT_SPIN);
-		return 1;
 	}
 
 	for (i = 0; INT_CONST[i].n; i++)
