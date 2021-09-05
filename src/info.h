243bc67e5bc77aecf11847579ca387afd4cfa07b
Ashnal <ashnal52@gmail.com>
2021-09-04 23:00:55 -0400

    Increase Mobj Freeslot limit to 1024 from 512

--- src/info.h
+++ src/info.h
@@ -567,7 +567,7 @@ void A_InvincSparkleRotate();
 extern boolean actionsoverridden[NUMACTIONS];
 
 // ratio of states to sprites to mobj types is roughly 6 : 1 : 1
-#define NUMMOBJFREESLOTS 512
+#define NUMMOBJFREESLOTS 1024
 #define NUMSPRITEFREESLOTS NUMMOBJFREESLOTS
 #define NUMSTATEFREESLOTS (NUMMOBJFREESLOTS*8)
 
