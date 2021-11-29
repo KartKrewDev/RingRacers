aaeaef5223efe520310d35ef19bf642319ba8082
James R <justsomejames2@gmail.com>
2021-11-29 14:40:30 -0800

    Do not set hu_stopped on interpolated frame

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -5264,12 +5264,14 @@ void TryRunTics(tic_t realtics)
 
 	if (neededtic > gametic)
 	{
-		hu_stopped = false;
+		if (realtics)
+			hu_stopped = false;
 	}
 
 	if (player_joining)
 	{
-		hu_stopped = true;
+		if (realtics)
+			hu_stopped = true;
 		return;
 	}
 
@@ -5307,7 +5309,8 @@ void TryRunTics(tic_t realtics)
 	}
 	else
 	{
-		hu_stopped = true;
+		if (realtics)
+			hu_stopped = true;
 	}
 }
 
