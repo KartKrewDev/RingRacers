9deeb79ac4fdbf07ed54643eecdc8843288640a3
toaster <rollerorbital@gmail.com>
2022-03-18 16:58:00 +0000

    My mistake, `P_PreTicker` calls `P_MapStart` and `P_MapEnd` too, so rework my earlier fix a bit
    # Conflicts:
    #	src/p_setup.c

--- src/p_setup.c
+++ src/p_setup.c
@@ -4304,6 +4304,8 @@ boolean P_LoadLevel(boolean fromnetsave, boolean reloadinggamestate)
 
 	P_RunCachedActions();
 
+	P_MapEnd(); // tmthing is no longer needed from this point onwards
+
 	// Took me 3 hours to figure out why my progression kept on getting overwritten with the titlemap...
 	if (!titlemapinaction)
 	{
@@ -4363,13 +4365,12 @@ boolean P_LoadLevel(boolean fromnetsave, boolean reloadinggamestate)
 				G_CopyTiccmd(&players[i].cmd, &netcmds[buf][i], 1);
 		}
 		P_PreTicker(2);
+		P_MapStart(); // just in case MapLoad modifies tmthing
 		LUAh_MapLoad();
+		P_MapEnd(); // just in case MapLoad modifies tmthing
 	}
 
-
 	K_TimerReset();
-
-	P_MapEnd(); // tmthing is no longer needed from this point onwards
 
 	// No render mode or reloading gamestate, stop here.
 	if (rendermode == render_none || reloadinggamestate)
