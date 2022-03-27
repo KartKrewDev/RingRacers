93dd4dc1194cbe739a38ce6f72779e121a263117
Sally Coolatta <tehrealsalt@gmail.com>
2022-03-27 00:25:27 -0400

    Give names to fpscap values

--- src/r_fps.c
+++ src/r_fps.c
@@ -25,13 +25,29 @@
 #endif
 
 static CV_PossibleValue_t fpscap_cons_t[] = {
-	{-1, "MIN"},
-	{1000, "MAX"},
-	//{0, "Uncapped"},
-	//{-1, "Refresh"},
+	{-1, "Match refresh rate"},
+	{0, "Unlimited"},
+#ifdef DEVELOP
+	// Lower values are actually pretty useful for debugging interp problems!
+	{1, "One Singular Frame"},
+	{10, "10"},
+	{20, "20"},
+	{25, "25"},
+	{30, "30"},
+#endif
+	{35, "35"},
+	{60, "60"},
+	{70, "70"},
+	{75, "75"},
+	{90, "90"},
+	{100, "100"},
+	{120, "120"},
+	{144, "144"},
+	{200, "200"},
+	{240, "240"},
 	{0, NULL}
 };
-consvar_t cv_fpscap = CVAR_INIT ("fpscap", "-1", CV_SAVE, fpscap_cons_t, NULL);
+consvar_t cv_fpscap = CVAR_INIT ("fpscap", "Match refresh rate", CV_SAVE, fpscap_cons_t, NULL);
 
 UINT32 R_GetFramerateCap(void)
 {
