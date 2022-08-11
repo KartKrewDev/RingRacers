607fe240bb1b6c81f66ab0c57e651b28f3c8d7b7
James R <justsomejames2@gmail.com>
2022-08-11 01:09:19 -0700

    Makefile: append branch name to EXENAME if not manually set

--- src/Makefile.d/win32.mk
+++ src/Makefile.d/win32.mk
@@ -2,11 +2,7 @@
 # Mingw, if you don't know, that's Win32/Win64
 #
 
-ifndef MINGW64
-EXENAME?=ringracers.exe
-else
-EXENAME?=ringracers64.exe
-endif
+exe:=$(exe)$(if $(MINGW64),64).exe
 
 # disable dynamicbase if under msys2
 ifdef MSYSTEM
