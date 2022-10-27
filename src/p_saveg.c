a1fd605a24a5c577ff89c48f3f918b41baebee5f
Eidolon <furyhunter600@gmail.com>
2022-10-27 18:45:16 -0500

    Read time fields from net saves

--- src/p_saveg.c
+++ src/p_saveg.c
@@ -4760,6 +4760,10 @@ static inline boolean P_NetUnArchiveMisc(boolean reloading)
 	starttime = READUINT32(save_p);
 	numbulbs = READUINT8(save_p);
 
+	timelimitintics = READUINT32(save_p);
+	extratimeintics = READUINT32(save_p);
+	secretextratime = READUINT32(save_p);
+
 	// Is it paused?
 	if (READUINT8(save_p) == 0x2f)
 		paused = true;
