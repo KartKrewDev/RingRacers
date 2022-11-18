70d83da50717bfde20b2b7d158ffad36a4775f95
toaster <rollerorbital@gmail.com>
2022-11-18 19:59:15 +0000

    More lenient waterskip per VC discussion

--- src/k_kart.c
+++ src/k_kart.c
@@ -3430,7 +3430,7 @@ boolean K_TripwirePass(player_t *player)
 
 boolean K_MovingHorizontally(mobj_t *mobj)
 {
-	return (P_AproxDistance(mobj->momx, mobj->momy) / 5 > abs(mobj->momz));
+	return (P_AproxDistance(mobj->momx, mobj->momy) / 4 > abs(mobj->momz));
 }
 
 boolean K_WaterRun(mobj_t *mobj)
