654f97fa72e2e301ab32f2626ea8ca26c540a278
James R <justsomejames2@gmail.com>
2023-02-12 02:15:23 -0800

    media: add WebM Vorbis and VP8 encoders

---
+++ src/media/webm_encoder.hpp
@@ -0,0 +1,51 @@
+// RING RACERS
+//-----------------------------------------------------------------------------
+// Copyright (C) 2023 by James Robert Roman
+//
+// This program is free software distributed under the
+// terms of the GNU General Public License, version 2.
+// See the 'LICENSE' file for more details.
+//-----------------------------------------------------------------------------
+
+#ifndef __SRB2_MEDIA_WEBM_ENCODER_HPP__
+#define __SRB2_MEDIA_WEBM_ENCODER_HPP__
+
+#include <mkvmuxer/mkvmuxer.h>
+
+#include "encoder.hpp"
+#include "webm_container.hpp"
+
+namespace srb2::media
+{
+
+template <typename T = mkvmuxer::Track>
+class WebmEncoder : virtual public MediaEncoder
+{
+public:
+	WebmEncoder(WebmContainer& container, webm::track trackid) : container_(container), trackid_(trackid)
+	{
+		container_.init_queue(trackid_);
+	}
+
+protected:
+	WebmContainer& container_;
+	webm::track trackid_;
+
+	std::size_t size() const { return container_.track_size(trackid_); }
+	time_unit_t duration() const { return container_.track_duration(trackid_); }
+
+	static T* get_track(const WebmContainer& container, webm::track trackid) { return container.get_track<T>(trackid); }
+
+	T* track() const { return get_track(container_, trackid_); }
+
+	virtual void write_frame(frame_buffer_t p, time_unit_t ts, bool is_key_frame) override final
+	{
+		const auto ts_nano = std::chrono::duration_cast<webm::duration>(ts);
+
+		container_.queue_frame(p, trackid_, ts_nano.count(), is_key_frame);
+	}
+};
+
+}; // namespace srb2::media
+
+#endif // __SRB2_MEDIA_WEBM_ENCODER_HPP__
---
+++ src/media/webm_vorbis.hpp
@@ -0,0 +1,59 @@
+// RING RACERS
+//-----------------------------------------------------------------------------
+// Copyright (C) 2023 by James Robert Roman
+//
+// This program is free software distributed under the
+// terms of the GNU General Public License, version 2.
+// See the 'LICENSE' file for more details.
+//-----------------------------------------------------------------------------
+
+#ifndef __SRB2_MEDIA_WEBM_VORBIS_HPP__
+#define __SRB2_MEDIA_WEBM_VORBIS_HPP__
+
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+#include "../cxxutil.hpp"
+#include "vorbis.hpp"
+#include "webm_encoder.hpp"
+
+namespace srb2::media
+{
+
+class WebmVorbisEncoder : public WebmEncoder<mkvmuxer::AudioTrack>, public VorbisEncoder
+{
+public:
+	WebmVorbisEncoder(WebmContainer& container, webm::track trackid, AudioEncoder::Config cfg) :
+		WebmEncoder(container, trackid), VorbisEncoder(cfg)
+	{
+		// write Vorbis extra data
+
+		const auto p = make_vorbis_private_data();
+
+		SRB2_ASSERT(track()->SetCodecPrivate(reinterpret_cast<const uint8_t*>(p.data()), p.size()) == true);
+	}
+
+	virtual BitRate estimated_bit_rate() const override final
+	{
+		auto _ = container_.queue_guard();
+
+		const std::chrono::duration<float> t = duration();
+
+		if (t <= t.zero())
+		{
+			return {};
+		}
+
+		using namespace std::chrono_literals;
+		return {static_cast<std::size_t>((size() * 8) / t.count()), 1s};
+	}
+
+private:
+	std::vector<std::byte> make_vorbis_private_data();
+};
+
+}; // namespace srb2::media
+
+#endif // __SRB2_MEDIA_WEBM_VORBIS_HPP__
