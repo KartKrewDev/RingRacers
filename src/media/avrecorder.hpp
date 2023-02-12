82251f6fb60653fbe6365ec2686ff31846cc1fc8
James R <justsomejames2@gmail.com>
2023-02-12 02:28:30 -0800

    media: add core AVRecorder
    Generic interface to audio and video encoders.

---
+++ src/media/avrecorder.hpp
@@ -0,0 +1,76 @@
+// RING RACERS
+//-----------------------------------------------------------------------------
+// Copyright (C) 2023 by James Robert Roman
+//
+// This program is free software distributed under the
+// terms of the GNU General Public License, version 2.
+// See the 'LICENSE' file for more details.
+//-----------------------------------------------------------------------------
+
+#ifndef __SRB2_MEDIA_AVRECORDER_HPP__
+#define __SRB2_MEDIA_AVRECORDER_HPP__
+
+#include <array>
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include <tcb/span.hpp>
+
+#include "../audio/sample.hpp"
+
+namespace srb2::media
+{
+
+class AVRecorder
+{
+public:
+	using audio_sample_t = srb2::audio::Sample<2>;
+	using audio_buffer_t = tcb::span<const audio_sample_t>;
+
+	class Impl;
+
+	struct Config
+	{
+		struct Audio
+		{
+			int sample_rate;
+		};
+
+		struct Video
+		{
+			int width;
+			int height;
+			int frame_rate;
+		};
+
+		std::string file_name;
+
+		std::optional<std::size_t> max_size; // file size limit
+		std::optional<std::chrono::duration<float>> max_duration;
+
+		std::optional<Audio> audio;
+		std::optional<Video> video;
+	};
+
+	AVRecorder(Config config);
+	~AVRecorder();
+
+	void push_audio_samples(audio_buffer_t buffer);
+
+	// True if this instance has terminated. Continuing to use
+	// this interface is useless and the object should be
+	// destructed immediately.
+	bool invalid() const;
+
+private:
+	std::unique_ptr<Impl> impl_;
+};
+
+}; // namespace srb2::media
+
+#endif // __SRB2_MEDIA_AVRECORDER_HPP__
---
+++ src/media/avrecorder_queue.cpp
@@ -0,0 +1,142 @@
+// RING RACERS
+//-----------------------------------------------------------------------------
+// Copyright (C) 2023 by James Robert Roman
+//
+// This program is free software distributed under the
+// terms of the GNU General Public License, version 2.
+// See the 'LICENSE' file for more details.
+//-----------------------------------------------------------------------------
+
+#include <chrono>
+#include <cstddef>
+#include <mutex>
+#include <utility>
+
+#include "avrecorder_impl.hpp"
+
+using namespace srb2::media;
+
+using Impl = AVRecorder::Impl;
+
+template <typename T>
+bool Impl::Queue<T>::advance(int new_pts, int duration)
+{
+	if (!impl_->valid_ || finished())
+	{
+		return false;
+	}
+
+	new_pts += duration;
+
+	// PTS must only advance.
+	if (new_pts <= pts())
+	{
+		return false;
+	}
+
+	auto finish = [this]
+	{
+		finished_ = true;
+
+		const auto t = impl_->container_->duration();
+
+		// Tracks are ultimately cut to the shortest among
+		// them, therefore it would be pointless for another
+		// queue to continue beyond this point.
+		//
+		// This is relevant if finishing due to size
+		// constraint; in that case, another queue might be
+		// far behind this one in terms of size and would
+		// continue in vain.
+
+		if (!impl_->max_duration_ || t < impl_->max_duration_)
+		{
+			impl_->max_duration_ = t;
+		}
+
+		impl_->wake_up_worker();
+	};
+
+	if (impl_->max_duration_)
+	{
+		const int final_pts = *impl_->max_duration_ / time_scale();
+
+		if (new_pts > final_pts)
+		{
+			return finish(), false;
+		}
+	}
+
+	if (impl_->max_size_)
+	{
+		const MediaEncoder::BitRate est = encoder_->estimated_bit_rate();
+
+		const float br = est.bits / 8.f;
+
+		// count size of already queued frames too
+		const float t = ((duration + queued_frames_) * time_scale()) / est.period;
+
+		if ((impl_->container_->size() + (t * br)) > *impl_->max_size_)
+		{
+			return finish(), false;
+		}
+	}
+
+	pts_ = new_pts;
+	queued_frames_ += duration;
+
+	return true;
+}
+
+Impl::QueueState Impl::encode_queues()
+{
+	bool remain = false;
+	bool flushed = false;
+
+	auto check = [&, this](auto& q, auto encode)
+	{
+		std::unique_lock lock(queue_mutex_);
+
+		if (!q.finished())
+		{
+			remain = true;
+		}
+
+		if (!q.vec_.empty())
+		{
+			const std::size_t n = q.queued_frames_;
+
+			auto copy = std::move(q.vec_);
+
+			lock.unlock();
+			encode(std::move(copy));
+			lock.lock();
+
+			q.queued_frames_ -= n;
+
+			flushed = true;
+		}
+	};
+
+	auto encode_audio = [this](auto copy) { audio_encoder_->encode(copy); };
+	auto encode_video = [this](auto copy) {};
+
+	check(audio_queue_, encode_audio);
+	check(video_queue_, encode_video);
+
+	if (flushed)
+	{
+		return QueueState::kFlushed;
+	}
+	else if (remain)
+	{
+		return QueueState::kEmpty;
+	}
+	else
+	{
+		return QueueState::kFinished;
+	}
+}
+
+template class Impl::Queue<AudioEncoder>;
+template class Impl::Queue<VideoEncoder>;
