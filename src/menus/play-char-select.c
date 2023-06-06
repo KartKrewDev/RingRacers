KartKrewDev/RingRacers

commit 8d9b42e49b6fda035379599b5304fa6c0c50a869

Skin name search-assistive hashing
More opportunities for early rejection in table-wide searches, in anticipation of future work.
- Many circumstances independently implemented string name comparisons. Most of these have been converted to use R_SkinAvailable, with one exception.
    - M_CharacterSelectInit already needs to iterate over the characters, so produce the skin hash independently.

src/menus/play-char-select.c
@@ -404,6 +404,8 @@ void M_CharacterSelectInit(void)
 	memset(setup_explosions, 0, sizeof(setup_explosions));
 	setup_animcounter = 0;
 
+	UINT32 localskinhash[MAXSPLITSCREENPLAYERS];
+
 	for (i = 0; i < MAXSPLITSCREENPLAYERS; i++)
 	{
 		// Default to no follower / match colour.
@@ -414,6 +416,9 @@ void M_CharacterSelectInit(void)
 		// Set default selected profile to the last used profile for each player:
 		// (Make sure we don't overshoot it somehow if we deleted profiles or whatnot)
 		setup_player[i].profilen = min(cv_lastprofile[i].value, PR_GetNumProfiles());
+
+		// Assistant for comparisons.
+		localskinhash[i] = quickncasehash(cv_skin[i].string, SKINNAMESIZE);
 	}
 
 	for (i = 0; i < numskins; i++)
@@ -436,7 +441,14 @@ void M_CharacterSelectInit(void)
 
 		for (j = 0; j < MAXSPLITSCREENPLAYERS; j++)
 		{
-			if (!strcmp(cv_skin[j].string, skins[i].name))
+			// See also R_SkinAvailable
+
+			if (localskinhash[j] != skins[i].namehash)
+				continue;
+
+			if (!stricmp(cv_skin[j].string, skins[i].name))
+				continue;
+
 			{
 				setup_player[j].gridx = x;
 				setup_player[j].gridy = y;

src/acs/call-funcs.cpp
@@ -264,16 +264,12 @@ static bool ACS_GetStateFromString(const char *word, statenum_t *type)
 --------------------------------------------------*/
 static bool ACS_GetSkinFromString(const char *word, INT32 *type)
 {
-	for (int i = 0; i < numskins; i++)
-	{
-		if (fastcmp(word, skins[i].name))
-		{
-			*type = i;
-			return true;
-		}
-	}
+	INT32 skin = R_SkinAvailable(word);
+	if (skin == -1)
+		return false;
 
-	return false;
+	*type = skin;
+	return true;
 }
 
 /*--------------------------------------------------

src/hardware/hw_md2.c
@@ -570,17 +570,15 @@ void HWR_InitModels(void)
 
 addskinmodel:
 		// add player model
-		for (s = 0; s < MAXSKINS; s++)
+		s = R_SkinAvailable(skinname);
+		if (s != -1)
 		{
-			if (stricmp(skinname, skins[s].name) == 0)
-			{
-				md2_playermodels[s].skin = s;
-				md2_playermodels[s].scale = scale;
-				md2_playermodels[s].offset = offset;
-				md2_playermodels[s].notfound = false;
-				strcpy(md2_playermodels[s].filename, filename);
-				goto modelfound;
-			}
+			md2_playermodels[s].skin = s;
+			md2_playermodels[s].scale = scale;
+			md2_playermodels[s].offset = offset;
+			md2_playermodels[s].notfound = false;
+			strcpy(md2_playermodels[s].filename, filename);
+			goto modelfound;
 		}
 
 		// no sprite/player skin name found?!?D

src/lua_hudlib.c
@@ -386,10 +386,8 @@ static int libd_getSprite2Patch(lua_State *L)
 	else // find skin by name
 	{
 		const char *name = luaL_checkstring(L, 1);
-		for (i = 0; i < numskins; i++)
-			if (fastcmp(skins[i].name, name))
-				break;
-		if (i >= numskins)
+		i = R_SkinAvailable(name);
+		if (i == -1)
 			return 0;
 	}
 

src/lua_mobjlib.c
@@ -662,17 +662,16 @@ static int mobj_set(lua_State *L)
 		break;
 	case mobj_skin: // set skin by name
 	{
-		INT32 i;
-		char skin[SKINNAMESIZE+1]; // all skin names are limited to this length
-		strlcpy(skin, luaL_checkstring(L, 3), sizeof skin);
-		strlwr(skin); // all skin names are lowercase
-		for (i = 0; i < numskins; i++)
-			if (fastcmp(skins[i].name, skin))
-			{
-				if (!mo->player || R_SkinUsable(mo->player-players, i, false))
-					mo->skin = &skins[i];
-				return 0;
-			}
+		const char *name = luaL_checkstring(L, 3);
+		INT32 skin = R_SkinAvailable(name);
+
+		if (skin != -1)
+		{
+			if (!mo->player || R_SkinUsable(mo->player-players, skin, false))
+				mo->skin = &skins[skin];
+
+			return 0;
+		}
 		return luaL_error(L, "mobj.skin '%s' not found!", skin);
 	}
 	case mobj_color:

src/lua_skinlib.c
@@ -196,12 +196,12 @@ static int lib_getSkin(lua_State *L)
 	}
 
 	// find skin by name
-	for (i = 0; i < numskins; i++)
-		if (fastcmp(skins[i].name, field))
-		{
-			LUA_PushUserdata(L, &skins[i], META_SKIN);
-			return 1;
-		}
+	i = R_SkinAvailable(field);
+	if (i != -1)
+	{
+		LUA_PushUserdata(L, &skins[i], META_SKIN);
+		return 1;
+	}
 
 	return 0;
 }

src/r_skins.c
@@ -314,12 +314,17 @@ UINT32 R_GetLocalRandomSkin(void)
 INT32 R_SkinAvailable(const char *name)
 {
 	INT32 i;
+	UINT32 hash = quickncasehash(name, SKINNAMESIZE);
 
 	for (i = 0; i < numskins; i++)
 	{
-		// search in the skin list
-		if (stricmp(skins[i].name,name)==0)
-			return i;
+		if (skins[i].namehash != hash)
+			continue;
+
+		if (stricmp(skins[i].name,name)!=0)
+			continue;
+
+		return i;
 	}
 	return -1;
 }
@@ -953,6 +958,8 @@ void R_AddSkins(UINT16 wadnum, boolean mainfile)
 						STRBUFCPY(skin->name, value2);
 					Z_Free(value2);
 				}
+
+				skin->namehash = quickncasehash(skin->name, SKINNAMESIZE);
 
 				// copy to hudname and fullname as a default.
 				if (!realname)

src/r_skins.h
@@ -37,7 +37,8 @@ extern "C" {
 /// The skin_t struct
 struct skin_t
 {
-	char name[SKINNAMESIZE+1]; // INT16 descriptive name of the skin
+	char name[SKINNAMESIZE+1]; // descriptive name of the skin
+	UINT32 namehash; // quickncasehash(->name, SKINNAMESIZE)
 	UINT16 wadnum;
 	skinflags_t flags;
 

commit 2aa36b5d326537a62138fba59a3fb8b0dbc4a24b

M_CharacterSelectInit: Fix a heaping of bugs
- Fix a softlock caused by commit 8d9b42e4 (!!)
- If your profile's skin was locked, fix a softlock where control was never restored
- If your profile's skin didn't exist, fix an invalid skin id set
- Just straight up flatten out the nested complexity of this function

src/menus/play-char-select.c
@@ -317,6 +317,9 @@ static void M_SetupProfileGridPos(setup_player_t *p)
 	INT32 i = R_SkinAvailable(pr->skinname);
 	INT32 alt = 0;	// Hey it's my character's name!
 
+	if (i == -1)
+		i = 0;
+
 	// While we're here, read follower values.
 	p->followern = K_FollowerAvailable(pr->follower);
 
@@ -349,6 +352,9 @@ static void M_SetupMidGameGridPos(setup_player_t *p, UINT8 num)
 	INT32 i = R_SkinAvailable(cv_skin[num].zstring);
 	INT32 alt = 0;	// Hey it's my character's name!
 
+	if (i == -1)
+		i = 0;
+
 	// While we're here, read follower values.
 	p->followern = cv_follower[num].value;
 	p->followercolor = cv_followercolor[num].value;
@@ -377,20 +383,6 @@ void M_CharacterSelectInit(void)
 	UINT8 i, j;
 	setup_maxpage = 0;
 
-	// While we're editing profiles, don't unset the devices for p1
-	if (gamestate == GS_MENU)
-	{
-		for (i = 0; i < MAXSPLITSCREENPLAYERS; i++)
-		{
-			// Un-set devices for all players if not editing profile
-			if (!optionsmenu.profile)
-			{
-				G_SetDeviceForPlayer(i, -1);
-				CONS_Printf("M_CharacterSelectInit: Device for %d set to %d\n", i, -1);
-			}
-		}
-	}
-
 	memset(setup_chargrid, -1, sizeof(setup_chargrid));
 	for (i = 0; i < 9; i++)
 	{
@@ -404,8 +396,6 @@ void M_CharacterSelectInit(void)
 	memset(setup_explosions, 0, sizeof(setup_explosions));
 	setup_animcounter = 0;
 
-	UINT32 localskinhash[MAXSPLITSCREENPLAYERS];
-
 	for (i = 0; i < MAXSPLITSCREENPLAYERS; i++)
 	{
 		// Default to no follower / match colour.
@@ -413,12 +403,33 @@ void M_CharacterSelectInit(void)
 		setup_player[i].followercategory = -1;
 		setup_player[i].followercolor = FOLLOWERCOLOR_MATCH;
 
-		// Set default selected profile to the last used profile for each player:
-		// (Make sure we don't overshoot it somehow if we deleted profiles or whatnot)
-		setup_player[i].profilen = min(cv_lastprofile[i].value, PR_GetNumProfiles());
+		// If we're on prpfile select, skip straight to CSSTEP_CHARS
+		// do the same if we're midgame, but make sure to consider splitscreen properly.
+		if (optionsmenu.profile && i == 0)
+		{
+			setup_player[i].profilen = optionsmenu.profilen;
+			//PR_ApplyProfileLight(setup_player[i].profilen, 0);
+			M_SetupProfileGridPos(&setup_player[i]);
+			setup_player[i].mdepth = CSSTEP_CHARS;
+		}
+		else
+		{
+			// Set default selected profile to the last used profile for each player:
+			// (Make sure we don't overshoot it somehow if we deleted profiles or whatnot)
+			setup_player[i].profilen = min(cv_lastprofile[i].value, PR_GetNumProfiles());
 
-		// Assistant for comparisons.
-		localskinhash[i] = quickncasehash(cv_skin[i].string, SKINNAMESIZE);
+			if (gamestate != GS_MENU && i <= splitscreen)
+			{
+				M_SetupMidGameGridPos(&setup_player[i], i);
+				setup_player[i].mdepth = CSSTEP_CHARS;
+			}
+			else
+			{
+				// Un-set devices
+				G_SetDeviceForPlayer(i, -1);
+				CONS_Printf("M_CharacterSelectInit: Device for %d set to %d\n", i, -1);
+			}
+		}
 	}
 
 	for (i = 0; i < numskins; i++)
@@ -437,43 +448,6 @@ void M_CharacterSelectInit(void)
 			setup_chargrid[x][y].numskins++;
 
 			setup_maxpage = max(setup_maxpage, setup_chargrid[x][y].numskins-1);
-		}
-
-		for (j = 0; j < MAXSPLITSCREENPLAYERS; j++)
-		{
-			// See also R_SkinAvailable
-
-			if (localskinhash[j] != skins[i].namehash)
-				continue;
-
-			if (!stricmp(cv_skin[j].string, skins[i].name))
-				continue;
-
-			{
-				setup_player[j].gridx = x;
-				setup_player[j].gridy = y;
-				setup_player[j].color = skins[i].prefcolor;
-
-				// If we're on prpfile select, skip straight to CSSTEP_CHARS
-				// do the same if we're midgame, but make sure to consider splitscreen properly.
-				if ((optionsmenu.profile && j == 0) || (gamestate != GS_MENU && j <= splitscreen))
-				{
-					if (optionsmenu.profile)	// In menu, setting up profile character/follower
-					{
-						setup_player[j].profilen = optionsmenu.profilen;
-						PR_ApplyProfileLight(setup_player[j].profilen, 0);
-						M_SetupProfileGridPos(&setup_player[j]);
-					}
-					else	// gamestate != GS_MENU, in that case, assume this is whatever profile we chose to play with.
-					{
-						setup_player[j].profilen = cv_lastprofile[j].value;
-						M_SetupMidGameGridPos(&setup_player[j], j);
-					}
-
-					// Don't reapply the profile here, it was already applied.
-					setup_player[j].mdepth = CSSTEP_CHARS;
-				}
-			}
 		}
 	}
 