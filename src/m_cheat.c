304ed85941d53bc61d18124155754ddd6f164b70
AJ Martinez <aj@worldsbe.st>
2023-07-01 16:14:42 -0700

    DEVELOP devmode cheat: allow saving

--- src/m_cheat.c
+++ src/m_cheat.c
@@ -146,7 +146,8 @@ static UINT8 cheatf_devmode(void)
 		gamedata->unlocked[i] = true;
 	}
 
-	G_SetUsedCheats();
+	// This is a developer feature, you know how to delete ringdata
+	// G_SetUsedCheats();
 	S_StartSound(0, sfx_kc42);
 
 	devparm = true;
