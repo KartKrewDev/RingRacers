785928c339b563f7c379381e027b8c314299da87
AJ Martinez <aj@worldsbe.st>
2023-09-12 04:14:49 -0700

    Anti farming: push the actual fucking code please

--- src/p_inter.c
+++ src/p_inter.c
@@ -389,6 +389,7 @@ void P_TouchSpecialThing(mobj_t *special, mobj_t *toucher, boolean heightcheck)
 			special->flags &= ~MF_SPECIAL;
 			return;
 		case MT_RANDOMITEM:
+			// -Wpedantic label wackness
 			UINT8 cheesetype = (special->flags2 & MF2_AMBUSH) ? 2 : 1;
 
 			if (!P_CanPickupItem(player, 1))
