9b86dbf8e049f74f8667a72b9294f563bb102ad6
James R <justsomejames2@gmail.com>
2023-11-13 01:15:05 -0800

    MT_DAYTONAPINETREE_SIDE: set punt_ref to center object

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -11321,6 +11321,7 @@ mobj_t *P_SpawnMobj(fixed_t x, fixed_t y, fixed_t z, mobjtype_t type)
 					mobj->y + FINESINE((ang>>ANGLETOFINESHIFT) & FINEMASK), mobj->z, MT_DAYTONAPINETREE_SIDE);
 				side->angle = ang;
 				P_SetTarget(&side->target, mobj);
+				P_SetTarget(&side->punt_ref, mobj);
 				side->threshold = i;
 			}
 			break;
