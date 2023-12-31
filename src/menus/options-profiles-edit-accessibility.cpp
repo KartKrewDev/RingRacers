95f82004586e29c96eacfa78ea6371170353da78
AJ Martinez <aj@worldsbe.st>
2023-12-31 00:01:04 -0700

    Fix screenshake cvar description overflowing screen

--- src/menus/options-profiles-edit-accessibility.cpp
+++ src/menus/options-profiles-edit-accessibility.cpp
@@ -116,7 +116,7 @@ menuitem_t OPTIONS_ProfileAccessibility[] = {
 	{IT_STRING | IT_CVAR, "Reduce Effects", "If overwhelmed, hide less-important particle cues.",
 		NULL, {.cvar = &cv_reducevfx}, 0, 0},
 
-	{IT_STRING | IT_CVAR, "Screenshake Strength", "Adjust shake intensity from damage, hazards, and offroad.",
+	{IT_STRING | IT_CVAR, "Screenshake Strength", "Adjust shake intensity from hazards and offroad.",
 		NULL, {.cvar = &cv_screenshake}, 0, 0},
 };
 
