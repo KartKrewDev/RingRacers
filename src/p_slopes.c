1d157c4e3c25b956d4d92369ef80b9f1bc69f781
AJ Martinez <aj@worldsbe.st>
2024-01-11 20:57:54 -0700

    Don't use Easy slope assist in tutorials

--- src/p_slopes.c
+++ src/p_slopes.c
@@ -1173,7 +1173,8 @@ void P_ButteredSlope(mobj_t *mo)
 
 		// Easy / Battle: SUPER NERF slope climbs, so that they're usually possible without resources.
 		// (New players suck at budgeting, and may not remember they have spindash / rings at all!)
-		if (gamespeed == KARTSPEED_EASY)
+		// Special exception for Tutorial because we're trying to teach slope mechanics there.
+		if (gamespeed == KARTSPEED_EASY && gametype != GT_TUTORIAL)
 		{
 			// Same as above, but use facing angle:
 			angle_t easyangle = mo->angle - mo->standingslope->xydirection;
