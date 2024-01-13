b6a725ad7bc75ceb79936814c677ab303e14cc5b
James R <justsomejames2@gmail.com>
2024-01-12 17:54:30 -0800

    HUD tracking: fix Super Flicky crash if owner died

--- src/k_hud_track.cpp
+++ src/k_hud_track.cpp
@@ -82,7 +82,7 @@ struct TargetTracking
 			return player_emeralds_color();
 
 		case MT_SUPER_FLICKY:
-			return static_cast<skincolornum_t>(Obj_SuperFlickyOwner(mobj)->color);
+			return Obj_SuperFlickyOwner(mobj) ? static_cast<skincolornum_t>(Obj_SuperFlickyOwner(mobj)->color) : SKINCOLOR_NONE;
 
 		default:
 			return SKINCOLOR_NONE;
