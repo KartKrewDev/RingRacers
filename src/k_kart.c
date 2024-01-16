92c451538af75896793d48384b1aca0a62123e54
James R <justsomejames2@gmail.com>
2024-01-15 16:30:12 -0800

    Special Stages: fade out music near the finish line

--- src/k_kart.c
+++ src/k_kart.c
@@ -10663,6 +10663,12 @@ void K_KartUpdatePosition(player_t *player)
 		player->topinfirst = 0;
 	}
 
+	// Special stages: fade out music near the finish line
+	if (P_IsLocalPlayer(player))
+	{
+		K_FadeOutSpecialMusic(player->distancetofinish);
+	}
+
 	player->position = position;
 }
 
