b97a94a2a9605793334c6b6992f898d92b183e48
AJ Martinez <aj@worldsbe.st>
2024-01-24 05:37:37 -0700

    Bumper inflate: remove debug print

--- src/p_map.c
+++ src/p_map.c
@@ -4177,9 +4177,9 @@ static void P_BouncePlayerMove(mobj_t *mo, TryMoveResult_t *result)
 	P_PlayerHitBounceLine(bestslideline, &result->normal);
 	mo->eflags |= MFE_JUSTBOUNCEDWALL;
 
+	// Combo avoidance!
 	if (mo->player && P_PlayerInPain(mo->player) && gametyperules & GTR_BUMPERS && mo->health > 1)
 	{
-		CONS_Printf("get me outta here\n");
 		P_DamageMobj(mo, NULL, NULL, 1, DMG_STUMBLE);
 	}
 
