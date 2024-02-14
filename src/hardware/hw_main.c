25ea0bf743deaaf91c92d902c7480056ed36ed64
AJ Martinez <aj@worldsbe.st>
2024-02-14 04:14:28 -0700

    Exclude all "3D Geometry" sprites from sprite-clip hack

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -3423,7 +3423,7 @@ static void HWR_SplitSprite(gl_vissprite_t *spr)
 
 	if (HWR_UseShader())
 	{
-		shader = (spr->mobj->frame & FF_PAPERSPRITE) ? SHADER_SPRITE : SHADER_SPRITECLIPHACK;
+		shader = (R_ThingIsPaperSprite(spr->mobj) || R_ThingIsFloorSprite(spr->mobj)) ? SHADER_SPRITE : SHADER_SPRITECLIPHACK;
 		blend |= PF_ColorMapped;
 	}
 
@@ -3916,7 +3916,7 @@ static void HWR_DrawSprite(gl_vissprite_t *spr)
 
 		if (HWR_UseShader())
 		{
-			shader = (spr->mobj->frame & FF_PAPERSPRITE) ? SHADER_SPRITE : SHADER_SPRITECLIPHACK;;
+			shader = (R_ThingIsPaperSprite(spr->mobj) || R_ThingIsFloorSprite(spr->mobj)) ? SHADER_SPRITE : SHADER_SPRITECLIPHACK;;
 			blend |= PF_ColorMapped;
 		}
 
