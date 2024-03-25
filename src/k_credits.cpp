19de024e21e657b4cdc860015fb44306c923c50d
James R <justsomejames2@gmail.com>
2024-03-24 18:09:01 -0700

    Delete experimental code committed by accident

--- src/k_credits.cpp
+++ src/k_credits.cpp
@@ -369,10 +369,6 @@ static void F_InitCreditsSlide(void)
 		}
 	}
 #endif
-	else if (slide->type == CRED_TYPE_KARTKREW)
-	{
-
-	}
 
 	// Clear the console hud just to avoid anything getting in the way.
 	CON_ClearHUD();
@@ -1224,9 +1220,4 @@ void F_CreditDrawer(void)
 		star.x += FixedMul(star.vel_x, renderdeltatics);
 		star.y += FixedMul(star.vel_y, renderdeltatics);
 	}
-}
-
-boolean F_CreditsRunning(void)
-{
-	return gamestate == GS_CREDITS || demo.attract == DEMO_ATTRACT_CREDITS;
 }
