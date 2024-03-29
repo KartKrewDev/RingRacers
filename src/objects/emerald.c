254597bece1165b56e8699ace4bae1ab5cc168d3
James R <justsomejames2@gmail.com>
2024-03-28 19:01:38 -0700

    Copyright fixup: James Robert Roman

--- src/objects/emerald.c
+++ src/objects/emerald.c
@@ -1,3 +1,13 @@
+// DR. ROBOTNIK'S RING RACERS
+//-----------------------------------------------------------------------------
+// Copyright (C) 2024 by James Robert Roman.
+// Copyright (C) 2024 by Kart Krew.
+//
+// This program is free software distributed under the
+// terms of the GNU General Public License, version 2.
+// See the 'LICENSE' file for more details.
+//-----------------------------------------------------------------------------
+
 #include "../k_battle.h"
 #include "../k_objects.h"
 #include "../k_specialstage.h"
--- src/objects/item-spot.c
+++ src/objects/item-spot.c
@@ -1,3 +1,12 @@
+// DR. ROBOTNIK'S RING RACERS
+//-----------------------------------------------------------------------------
+// Copyright (C) 2024 by James Robert Roman.
+//
+// This program is free software distributed under the
+// terms of the GNU General Public License, version 2.
+// See the 'LICENSE' file for more details.
+//-----------------------------------------------------------------------------
+
 #include "../doomdef.h"
 #include "../m_fixed.h"
 #include "../k_objects.h"
