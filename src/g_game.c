e9205e19d67e64a05e3400b5a2a42fc2a9238e96
toaster <rollerorbital@gmail.com>
2024-03-30 15:56:44 +0000

    ACS_RunPlayerEnterScript: If betweenmaps, defer until P_PostLoadLevel
    Guarantees that the ENTER script actually consistently runs, since previously it was happening before ACS scope invalidation

--- src/g_game.c
+++ src/g_game.c
@@ -2519,9 +2519,9 @@ void G_PlayerReborn(INT32 player, boolean betweenmaps)
 		}
 	}
 
-	if (p->spectator == false)
+	if (p->spectator == false && !betweenmaps)
 	{
-		if (betweenmaps || enteredGame == true)
+		if (enteredGame == true)
 		{
 			ACS_RunPlayerEnterScript(p);
 		}
@@ -2530,15 +2530,6 @@ void G_PlayerReborn(INT32 player, boolean betweenmaps)
 			ACS_RunPlayerRespawnScript(p);
 		}
 	}
-
-	if (betweenmaps)
-		return;
-
-	if (leveltime < starttime)
-		return;
-
-	if (exiting)
-		return;
 }
 
 //
