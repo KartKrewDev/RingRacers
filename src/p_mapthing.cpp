7f6de407f17b16deaa6a0d0fc9105aac27926741
toaster <rollerorbital@gmail.com>
2024-04-02 17:42:38 +0100

    Copyright update: Add Kart Krew (2024) in general for high-traffic inherited sourcefiles

--- src/p_mapthing.cpp
+++ src/p_mapthing.cpp
@@ -1,14 +1,16 @@
 // DR. ROBOTNIK'S RING RACERS
 //-----------------------------------------------------------------------------
-// Copyright (C) 1993-1996 by id Software, Inc.
-// Copyright (C) 1998-2000 by DooM Legacy Team.
-// Copyright (C) 1999-2020 by Sonic Team Junior.
-// Copyright (C) 2024      by Kart Krew.
+// Copyright (C) 2024 by Kart Krew.
+// Copyright (C) 2020 by Sonic Team Junior.
+// Copyright (C) 2000 by DooM Legacy Team.
+// Copyright (C) 1996 by id Software, Inc.
 //
 // This program is free software distributed under the
 // terms of the GNU General Public License, version 2.
 // See the 'LICENSE' file for more details.
 //-----------------------------------------------------------------------------
+/// \file  p_mapthing.cpp
+/// \brief Mapthing spawning assistance
 
 #include <algorithm>
 #include <iterator>
