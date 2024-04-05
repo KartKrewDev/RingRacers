2523288a552bff44583df867cc485430ba678122
Sally Coolatta <tehrealsalt@gmail.com>
2024-04-05 12:02:13 -0400

    Changed my mind, MODVERSION should definitely be 1

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 // it's only for detection of the version the player is using so the MS can alert them of an update.
 // Only set it higher, not lower, obviously.
 // Note that we use this to help keep internal testing in check; this is why v2.0 is not version "2".
-#define MODVERSION 11
+#define MODVERSION 1
 
 // Define this as a prerelease version suffix
 // #define BETAVERSION "Alpha"
