a7a0c31d8ccbfdbd28e589b6b0450e1b82420eef
Sally Coolatta <tehrealsalt@gmail.com>
2024-04-05 09:49:36 -0400

    SRB2 -> DRRR copyright in dummy folder

--- src/dummy/i_system.c
+++ src/dummy/i_system.c
@@ -1,3 +1,15 @@
+// DR. ROBOTNIK'S RING RACERS
+//-----------------------------------------------------------------------------
+// Copyright (C) 2024 by Kart Krew.
+// Copyright (C) 2020 by Sonic Team Junior.
+// Copyright (C) 2000 by DooM Legacy Team.
+// Copyright (C) 1996 by id Software, Inc.
+//
+// This program is free software distributed under the
+// terms of the GNU General Public License, version 2.
+// See the 'LICENSE' file for more details.
+//-----------------------------------------------------------------------------
+
 #include "../doomdef.h"
 #include "../doomtype.h"
 #include "../i_system.h"
