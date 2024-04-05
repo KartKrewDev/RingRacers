4e72836ea1e2c15f6ee08f924625ab34909e04ed
Sally Coolatta <tehrealsalt@gmail.com>
2024-04-05 11:41:39 -0400

    Update version numbers
    MODVERSION could be changed to 1 because RR has a different application name already, but I made it increment from SRB2Kart's current value anyways to be safe.

--- src/version.h
+++ src/version.h
@@ -1,15 +1,10 @@
-#define SRB2VERSION "1.0"/* this must be the first line, for cmake !! */
-
-// The Modification ID; must be obtained from a Master Server Admin ( https://mb.srb2.org/showgroups.php ).
-// DO NOT try to set this otherwise, or your modification will be unplayable through the Master Server.
-// "17" is the default mod ID for SRB2Kart.
-#define MODID 17
+#define SRB2VERSION "2.0"/* this must be the first line, for cmake !! */
 
 // The Modification Version, starting from 1. Do not follow your version string for this,
 // it's only for detection of the version the player is using so the MS can alert them of an update.
 // Only set it higher, not lower, obviously.
 // Note that we use this to help keep internal testing in check; this is why v2.0 is not version "2".
-#define MODVERSION 6
+#define MODVERSION 11
 
 // Define this as a prerelease version suffix
 // #define BETAVERSION "Alpha"
