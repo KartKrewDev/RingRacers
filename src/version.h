03241a13c5b22f567577c2cb06c4bbfb2f8e3cc9
Eidolon <furyhunter600@gmail.com>
2024-05-20 23:36:25 -0500

    Bump MODVERSION for 2.3

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
-#define SRB2VERSION "2.2"/* this must be the first line, for cmake !! */
+#define SRB2VERSION "2.3"/* this must be the first line, for cmake !! */
 
 // The Modification Version, starting from 1. Do not follow your version string for this,
 // it's only for detection of the version the player is using so the MS can alert them of an update.
 // Only set it higher, not lower, obviously.
 // Note that we use this to help keep internal testing in check; this is why v2.0 is not version "2".
-#define MODVERSION 3
+#define MODVERSION 4
 
 // Define this as a prerelease version suffix
 // #define BETAVERSION "Alpha"
