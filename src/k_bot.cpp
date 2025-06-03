55d63fb40286e7a477d4ab45cf167a35533e03db
Antonio Martinez <aj@worldsbe.st>
2025-06-03 13:02:04 -0400

    Allow complexity to drop a little lower

--- src/k_bot.cpp
+++ src/k_bot.cpp
@@ -581,7 +581,9 @@ const botcontroller_t *K_GetBotController(const mobj_t *mobj)
 fixed_t K_BotMapModifier(void)
 {
 	constexpr INT32 complexity_scale = 10000;
-	constexpr fixed_t modifier_max = FRACUNIT * 2;
+	fixed_t modifier_max = FRACUNIT * 2;
+	fixed_t modifier_min = 4 * FRACUNIT / 10;
+	modifier_min -= FRACUNIT;
 
 	const fixed_t complexity_value = std::clamp<fixed_t>(
 		FixedDiv(K_GetTrackComplexity(), complexity_scale),
