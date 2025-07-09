8f695d4aac704402ea51168944c2985586c5afd8
VelocitOni <velocitoni@gmail.com>
2025-07-08 23:48:56 -0400

    Raise Bail Boost Speed
    I FUCKING MERGED IT WITHOUT PUSHING THE COMMITS I BUILT AND HANDED TO THE TESTERS

--- src/k_kart.h
+++ src/k_kart.h
@@ -46,7 +46,7 @@ Make sure this matches the actual number of states
 
 #define BAIL_MAXCHARGE (84) // tics to bail when in painstate nad in air, on ground is half, if you touch this, also update Obj_BailChargeThink synced animation logic
 #define BAIL_DROP (FRACUNIT)
-#define BAIL_BOOST (FRACUNIT)
+#define BAIL_BOOST (6*FRACUNIT/5) 
 #define BAIL_CREDIT_DEBTRINGS (true)
 #define BAIL_DROPFREQUENCY (2)
 #define BAILSTUN (TICRATE*7)
