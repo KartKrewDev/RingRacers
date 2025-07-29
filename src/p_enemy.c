d2b7bfcc0049974c38738fa0be892bc619f4e1f5
VelocitOni <velocitoni@gmail.com>
2025-07-29 03:12:41 -0400

    Big fat quakes
    It quakes, and proxmine explosion is fat too

--- src/p_enemy.c
+++ src/p_enemy.c
@@ -12207,6 +12207,8 @@ void A_BallhogExplode(mobj_t *actor)
 	P_SetTarget(&mo2->target, actor->target);
 	S_StartSound(mo2, actor->info->deathsound);
 	
+	P_StartQuakeFromMobj(7, 50 * actor->scale, 1024 * actor->scale, actor);
+
 	actor->fuse = 1;
 	return;
 }
@@ -12217,7 +12219,7 @@ void A_SpecialStageBombExplode(mobj_t *actor)
 		return;
 
 	K_SpawnLandMineExplosion(actor, SKINCOLOR_KETCHUP, actor->hitlag);
-	P_StartQuakeFromMobj(TICRATE/6, 24 * actor->scale, 512 * mapobjectscale, actor);
+	P_StartQuakeFromMobj(7, 80 * actor->scale, 2048 * mapobjectscale, actor);
 }
 
 // A_LightningFollowPlayer:
