c5b3f6ac22efe9fe2899490537ff8d17d67e43f8
JugadorXEI <eugeniom9@gmail.com>
2025-08-08 16:03:46 +0200

    Expose K_PressingEBrake to Lua

--- src/lua_baselib.c
+++ src/lua_baselib.c
@@ -3797,6 +3797,16 @@ static int lib_kGenericExtraFlagsNoZAdjust(lua_State *L)
 	return 0;
 }
 
+static int lib_kPressingEBrake(lua_State *L)
+{
+	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
+	INLEVEL
+	if (!player)
+		return LUA_ErrInvalid(L, "player_t");
+	lua_pushboolean(L, K_PressingEBrake(player));
+	return 1;
+}
+
 static int lib_kDoInstashield(lua_State *L)
 {
 	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
@@ -4670,6 +4680,7 @@ static luaL_Reg lib[] = {
 	{"K_SpawnInvincibilitySpeedLines",lib_kSpawnInvincibilitySpeedLines},
 	{"K_SpawnBumpEffect",lib_kSpawnBumpEffect},
 	{"K_GenericExtraFlagsNoZAdjust",lib_kGenericExtraFlagsNoZAdjust},
+	{"K_PressingEBrake",lib_kPressingEBrake},
 	{"K_DoInstashield",lib_kDoInstashield},
 	{"K_SpawnBattlePoints",lib_kSpawnBattlePoints},
 	{"K_SpinPlayer",lib_kSpinPlayer},
