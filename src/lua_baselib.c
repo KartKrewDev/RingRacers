ceb21535c382b3fd8e89fcceee219b70b05eaf33
JugadorXEI <eugeniom9@gmail.com>
2025-08-08 16:07:49 +0200

    Expose K_MomentumAngleEx to Lua

--- src/lua_baselib.c
+++ src/lua_baselib.c
@@ -3807,6 +3807,17 @@ static int lib_kPressingEBrake(lua_State *L)
 	return 1;
 }
 
+static int lib_kMomentumAngleEx(lua_State *L)
+{
+	mobj_t *mo = *((mobj_t **)luaL_checkudata(L, 1, META_MOBJ));
+	fixed_t threshold = luaL_checkfixed(L, 2);
+	INLEVEL
+	if (!mo)
+		return LUA_ErrInvalid(L, "mobj_t");
+	lua_pushangle(L, K_MomentumAngleEx(mo, threshold));
+	return 1;
+}
+
 static int lib_kDoInstashield(lua_State *L)
 {
 	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
@@ -4681,6 +4692,7 @@ static luaL_Reg lib[] = {
 	{"K_SpawnBumpEffect",lib_kSpawnBumpEffect},
 	{"K_GenericExtraFlagsNoZAdjust",lib_kGenericExtraFlagsNoZAdjust},
 	{"K_PressingEBrake",lib_kPressingEBrake},
+	{"K_MomentumAngleEx",lib_kMomentumAngleEx},
 	{"K_DoInstashield",lib_kDoInstashield},
 	{"K_SpawnBattlePoints",lib_kSpawnBattlePoints},
 	{"K_SpinPlayer",lib_kSpinPlayer},
