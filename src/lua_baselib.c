9ec314aac4e5e6d5eaee2fc897e06db90b074e3a
JugadorXEI <eugeniom9@gmail.com>
2025-08-09 18:06:42 +0200

    Expose K_DefaultPlayerRadius to Lua

--- src/lua_baselib.c
+++ src/lua_baselib.c
@@ -4647,6 +4647,16 @@ static int lib_kPlayerBaseFriction(lua_State *L)
 	return 1;
 }
 
+static int lib_kDefaultPlayerRadius(lua_State *L)
+{
+	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
+	INLEVEL
+	if (!player)
+		return LUA_ErrInvalid(L, "player_t");
+	lua_pushfixed(L, player->mo ? K_DefaultPlayerRadius(player) : -1);
+	return 1;
+}
+
 static int lib_kGetCollideAngle(lua_State *L)
 {
 	mobj_t *t1 = *((mobj_t **)luaL_checkudata(L, 1, META_MOBJ));
@@ -5123,6 +5133,7 @@ static luaL_Reg lib[] = {
 	{"K_PlayerEBrake",lib_kPlayerEBrake},
 	{"K_Sliptiding",lib_kSliptiding},
 	{"K_PlayerBaseFriction",lib_kPlayerBaseFriction},
+	{"K_DefaultPlayerRadius",lib_kDefaultPlayerRadius},
 
 	{"K_GetCollideAngle",lib_kGetCollideAngle},
 	{"K_AddHitLag",lib_kAddHitLag},
