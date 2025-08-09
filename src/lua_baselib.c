641630bbdd0f4c7ddfbdec1435dddd4c60d34bb8
JugadorXEI <eugeniom9@gmail.com>
2025-08-09 18:10:12 +0200

    Expose K_ItemScaleForPlayer to Lua

--- src/lua_baselib.c
+++ src/lua_baselib.c
@@ -4657,6 +4657,16 @@ static int lib_kDefaultPlayerRadius(lua_State *L)
 	return 1;
 }
 
+static int lib_kItemScaleForPlayer(lua_State *L)
+{
+	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
+	INLEVEL
+	if (!player)
+		return LUA_ErrInvalid(L, "player_t");
+	lua_pushfixed(L, K_ItemScaleForPlayer(player));
+	return 1;
+}
+
 static int lib_kGetCollideAngle(lua_State *L)
 {
 	mobj_t *t1 = *((mobj_t **)luaL_checkudata(L, 1, META_MOBJ));
@@ -5134,6 +5144,7 @@ static luaL_Reg lib[] = {
 	{"K_Sliptiding",lib_kSliptiding},
 	{"K_PlayerBaseFriction",lib_kPlayerBaseFriction},
 	{"K_DefaultPlayerRadius",lib_kDefaultPlayerRadius},
+	{"K_ItemScaleForPlayer",lib_kItemScaleForPlayer},
 
 	{"K_GetCollideAngle",lib_kGetCollideAngle},
 	{"K_AddHitLag",lib_kAddHitLag},
