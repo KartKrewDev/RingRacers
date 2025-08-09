70c33757d3ade026bda7d87067384d031991beab
JugadorXEI <eugeniom9@gmail.com>
2025-08-09 12:23:42 +0200

    Expose K_CheckPlayersRespawnColliding to Lua

--- src/lua_baselib.c
+++ src/lua_baselib.c
@@ -4166,6 +4166,18 @@ static int lib_kFindJawzTarget(lua_State *L)
 	return 1;
 }
 
+static int lib_kCheckPlayersRespawnColliding(lua_State *L)
+{
+	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
+	fixed_t x = luaL_checkinteger(L, 2);
+	fixed_t y = luaL_checkinteger(L, 3);
+	INLEVEL
+	if (!player)
+		return LUA_ErrInvalid(L, "player_t");
+	lua_pushboolean(L, K_CheckPlayersRespawnColliding(player-players, x, y));
+	return 1;
+}
+
 static int lib_kGetKartDriftSparkValue(lua_State *L)
 {
 	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
@@ -4787,6 +4799,7 @@ static luaL_Reg lib[] = {
 	{"K_KillBananaChain",lib_kKillBananaChain},
 	{"K_RepairOrbitChain",lib_kRepairOrbitChain},
 	{"K_FindJawzTarget",lib_kFindJawzTarget},
+	{"K_CheckPlayersRespawnColliding",lib_kCheckPlayersRespawnColliding},
 	{"K_GetKartDriftSparkValue",lib_kGetKartDriftSparkValue},
 	{"K_StairJankFlip",lib_kStairJankFlip},
 	{"K_SpawnDriftBoostExplosion",lib_kSpawnDriftBoostExplosion},
