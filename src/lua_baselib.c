e687e6a0f1346e991a0fa2ecb78e9c533e5ca572
JugadorXEI <eugeniom9@gmail.com>
2025-08-09 16:08:00 +0200

    Expose K_GetSpindashChargeTime to Lua

--- src/lua_baselib.c
+++ src/lua_baselib.c
@@ -4448,6 +4448,16 @@ static int lib_kGetGardenTop(lua_State *L)
 	return 1;
 }
 
+static int lib_kGetSpindashChargeTime(lua_State *L)
+{
+	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
+	INLEVEL
+	if (!player)
+		return LUA_ErrInvalid(L, "player_t");
+	lua_pushinteger(L, K_GetSpindashChargeTime(player));
+	return 1;
+}
+
 static int lib_kGetKartSpeed(lua_State *L)
 {
 	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
@@ -4972,6 +4982,7 @@ static luaL_Reg lib[] = {
 	{"K_IsRidingFloatingTop",lib_kIsRidingFloatingTop},
 	{"K_IsHoldingDownTop",lib_kIsHoldingDownTop},
 	{"K_GetGardenTop",lib_kGetGardenTop},
+	{"K_GetSpindashChargeTime",lib_kGetSpindashChargeTime},
 	{"K_GetKartSpeed",lib_kGetKartSpeed},
 	{"K_GetKartAccel",lib_kGetKartAccel},
 	{"K_GetKartFlashing",lib_kGetKartFlashing},
