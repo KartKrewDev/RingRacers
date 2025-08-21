905d4a13bf6cbbc282206f4ff1f2ef3ac398e77e
JugadorXEI <eugeniom9@gmail.com>
2025-08-21 09:59:13 +0200

    Expose K_ApplyOffroad to Lua

--- src/lua_baselib.c
+++ src/lua_baselib.c
@@ -4338,6 +4338,16 @@ static int lib_kSpawnWaterRunParticles(lua_State *L)
 	return 0;
 }
 
+static int lib_kApplyOffroad(lua_State *L)
+{
+	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
+	INLEVEL
+	if (!player)
+		return LUA_ErrInvalid(L, "player_t");
+	lua_pushboolean(L, K_ApplyOffroad(player));
+	return 1;
+}
+
 static int lib_kGetKartSpeed(lua_State *L)
 {
 	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
@@ -4851,6 +4861,7 @@ static luaL_Reg lib[] = {
 	{"K_StripOther",lib_kStripOther},
 	{"K_MomentumToFacing",lib_kMomentumToFacing},
 	{"K_SpawnWaterRunParticles",lib_kSpawnWaterRunParticles},
+	{"K_ApplyOffroad",lib_kApplyOffroad},
 	{"K_GetKartSpeed",lib_kGetKartSpeed},
 	{"K_GetKartAccel",lib_kGetKartAccel},
 	{"K_GetKartFlashing",lib_kGetKartFlashing},
