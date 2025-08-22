c831549bbd5d26d8600500fc5edc74b52803d624
JugadorXEI <eugeniom9@gmail.com>
2025-08-22 09:01:42 +0200

    Expose K_FastFallBounce to Lua

--- src/lua_baselib.c
+++ src/lua_baselib.c
@@ -5048,6 +5048,17 @@ static int lib_kSetItemOut(lua_State *L)
 	return 0;
 }
 
+static int lib_kFastFallBounce(lua_State *L)
+{
+	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
+	NOHUD
+	INLEVEL
+	if (!player)
+		return LUA_ErrInvalid(L, "player_t");
+	lua_pushboolean(L, K_FastFallBounce(player));
+	return 1;
+}
+
 static int lib_kUnsetItemOut(lua_State *L)
 {
 	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
@@ -5947,6 +5958,7 @@ static luaL_Reg lib[] = {
 	{"K_ThunderDome",lib_kThunderDome},
 	{"K_PlayerCanUseItem",lib_kPlayerCanUseItem},
 	{"K_PlayerGuard",lib_kPlayerGuard},
+	{"K_FastFallBounce",lib_kFastFallBounce},
 	{"K_SetTireGrease",lib_kSetTireGrease},
 
 	{"K_GetCollideAngle",lib_kGetCollideAngle},
