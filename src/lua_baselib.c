8fecb320cb3709d96c8c432c52533ca1fa60e8d5
JugadorXEI <eugeniom9@gmail.com>
2025-08-22 09:02:16 +0200

    Expose K_EggmanTransfer to Lua

--- src/lua_baselib.c
+++ src/lua_baselib.c
@@ -5160,6 +5160,18 @@ static int lib_kPlayerCanUseItem(lua_State *L)
 	return 1;
 }
 
+static int lib_kEggmanTransfer(lua_State *L)
+{
+	player_t *source = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
+	player_t *victim = *((player_t **)luaL_checkudata(L, 2, META_PLAYER));
+	NOHUD
+	INLEVEL
+	if (!source || !victim)
+		return LUA_ErrInvalid(L, "player_t");
+	K_EggmanTransfer(source, victim);
+	return 0;
+}
+
 static int lib_kSetTireGrease(lua_State *L)
 {
 	player_t *player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
@@ -5959,6 +5971,7 @@ static luaL_Reg lib[] = {
 	{"K_PlayerCanUseItem",lib_kPlayerCanUseItem},
 	{"K_PlayerGuard",lib_kPlayerGuard},
 	{"K_FastFallBounce",lib_kFastFallBounce},
+	{"K_EggmanTransfer",lib_kEggmanTransfer},
 	{"K_SetTireGrease",lib_kSetTireGrease},
 
 	{"K_GetCollideAngle",lib_kGetCollideAngle},
