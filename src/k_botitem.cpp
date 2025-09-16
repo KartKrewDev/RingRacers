32a897233e7d80543ea45b9273abe12b04def3d7
Antonio Martinez <aj@worldsbe.st>
2025-09-15 21:27:14 -0400

    Orbital timeouts 25sec to 10sec

--- src/k_botitem.cpp
+++ src/k_botitem.cpp
@@ -950,7 +950,7 @@ static void K_BotItemOrbinaut(const player_t *player, ticcmd_t *cmd)
 		cmd->buttons |= BT_LOOKBACK;
 	}
 
-	if (player->botvars.itemconfirm > 25*TICRATE)
+	if (player->botvars.itemconfirm > 10*TICRATE)
 	{
 		K_BotGenericPressItem(player, cmd, throwdir);
 	}
@@ -1178,7 +1178,7 @@ static void K_BotItemJawz(const player_t *player, ticcmd_t *cmd)
 		cmd->buttons |= BT_LOOKBACK;
 	}
 
-	if (player->botvars.itemconfirm > 25*TICRATE)
+	if (player->botvars.itemconfirm > 10*TICRATE)
 	{
 		K_BotGenericPressItem(player, cmd, throwdir);
 	}
