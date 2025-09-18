d6919df0f9727d1abaa22afb156a0fe5c1d6ed87
Eidolon <furyhunter600@gmail.com>
2025-09-17 22:20:18 -0500

    Start rc4 modversion 7

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 // it's only for detection of the version the player is using so the MS can alert them of an update.
 // Only set it higher, not lower, obviously.
 // Note that we use this to help keep internal testing in check; this is why v2.0 is not version "2".
-#define MODVERSION 6
+#define MODVERSION 7
 
 // Define this as a prerelease version suffix
-#define BETAVERSION "RC3"
+#define BETAVERSION "RC4"
