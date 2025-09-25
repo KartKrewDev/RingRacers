e9906915b03c49518074ee5d3c71f637a7ae7e50
Eidolon <furyhunter600@gmail.com>
2025-09-24 19:56:20 -0500

    Calculate podium position for PODIUMCUP condition instead of using current player position
    Fixes https://gitlab.com/kart-krew-dev/ring-racers/-/issues/253

--- src/m_cond.c
+++ src/m_cond.c
@@ -1843,8 +1843,7 @@ boolean M_CheckCondition(condition_t *cn, player_t *player)
 			if (cn->extrainfo2 != 0)
 				return (K_PodiumGrade() >= cn->extrainfo1);
 			if (cn->extrainfo1 != 0)
-				return (player->position != 0
-					&& player->position <= cn->extrainfo1);
+				return K_GetPodiumPosition(player) <= cn->extrainfo1;
 			return true;
 		case UCRP_PODIUMEMERALD:
 		case UCRP_PODIUMPRIZE:
