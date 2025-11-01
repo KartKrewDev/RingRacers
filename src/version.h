c6bcc403ba5d10cdc495702c59db17507fa87c85
Eidolon <furyhunter600@gmail.com>
2025-10-31 20:20:09 -0500

    Bump modversion for 2.4 final

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 // it's only for detection of the version the player is using so the MS can alert them of an update.
 // Only set it higher, not lower, obviously.
 // Note that we use this to help keep internal testing in check; this is why v2.0 is not version "2".
-#define MODVERSION 12
+#define MODVERSION 13
 
 // Define this as a prerelease version suffix
-#define BETAVERSION "RC9"
+//#define BETAVERSION "RC10"
